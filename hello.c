01d5369e00b9841a24f7eaed33e27f8bcbb1fdc2
ddelsol47 <srumsey2@huskers.unl.edu>
2022-08-25 03:43:28 +0000

    MAJOR BAJOR

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Setheon Rumsey / ddelsol47
+ * srumsey2@huskers.unl.edu
+ * Date: 2022/8/24
+ *
+ * HELL LO WORLB
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("setheon rumsey is ma name.\n");
+  printf("computer science is ma game.\n");
+
+  return 0;
+}
